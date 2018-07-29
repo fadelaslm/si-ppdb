16029842294fe821114d07b4f49cf2a467acfc2d
fadel_aslm <fadel_aslm.fadelisme08@gmail.com>
2018-07-29 20:20:45 +0700

    Commit method auth from controller Login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array('database', 'session','upload','user_agent',
-'form_validation', 'recaptcha');
+'form_validation', 'recaptcha', 'pagination', 'email');
 
 /*
 | -------------------------------------------------------------------
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form');
+$autoload['helper'] = array('url', 'form', 'prodi', 'text', 'provkota');
 
 /*
 | -------------------------------------------------------------------
--- application/controllers/authentication/Login.php
+++ application/controllers/authentication/Login.php
@@ -11,12 +11,16 @@
     parent::__construct();
     $this->load->model('authentication/auth');
     $this->load->model('app/m_dashboard');
+    $this->load->library('recaptcha');
+    $this->load->library('session');
+    $this->load->library(array('session'));
+    $this->load->helper(array('url'));
    }
 
    public function index()
    {
      $data = array(
-       'title' => 'Login Page',
+       'title' => 'Login Panel Admin PPDB SMK Dinamika Pembangunan 1 Jakarta',
        'eror'  => ''
      );
      $this->load->view('authentication/login', $data);
@@ -24,39 +28,48 @@
 
    public function auth()
    {
+
+     $this->load->helper(array('form', 'url'));
+     $this->load->library('form_validation');
      // Google recaptcha
      $captcha_answer = $this->input->post('g-recaptcha-response');
      $response       = $this->recaptcha->verifyResponse($captcha_answer);
      //End Google Recaptcha
-
-     if ($this->input->post() AND $response['success'] )
+     if ($this->input->post())
      {
-       $auth = $this->auth->authentication($this->input->post())->result_array();
+        $this->form_validation->set_rules('username', 'Username', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|xss_clean');
+        if($this->form_validation->run() == TRUE)
+        {
+          redirect('auth/login');
+        }
 
-       if($auth != NULL)
-       {
-         $data = array(
-           'id'       => $auth[0]['id'],
-           'fullname' => $auth[0]['fullname']
-         );
+        $auth = $this->auth->authentication($this->input->post())->result_array();
+        if($auth != NULL)
+          {
+            $data = array(
+              'id'       => $auth[0]['id'],
+              'fullname' => $auth[0]['fullname'],
+              'status'   => $auth[0]['role']
+            );
 
-         $this->session->set_userdata('logeddin', $data);
-         redirect('app/dashboard');
-       } else {
-         $data = array(
-           'title' => 'Login Page',
-           'eror'  => '
-               <script>swal({
-                     title: "Gagal!",
-                     text: "Login Gagal, Periksa Username dan Password Anda.",
-                     type: "error",
-                     confirmButtonText: "OK"
-                     });</script>
-             ',
-         );
-         $this->load->view('authentication/login', $data);
-       }
-     }  else {
+            $this->session->set_userdata('logeddin', $data);
+            redirect('app/dashboard');
+          } else {
+            $data = array(
+              'title' => 'Login Page',
+              'eror'  => '
+                  <script>swal({
+                        title: "Gagal!",
+                        text: "Login Gagal, Periksa Username dan Password Anda.",
+                        type: "error",
+                        confirmButtonText: "OK"
+                        });</script>
+                ',
+            );
+            $this->load->view('auth/login', $data);
+          }
+      } else {
        $data = array(
         'title'			=> 'Login GAGAL',
         'eror'			=> '
@@ -68,7 +81,7 @@
                   });</script>
           ',
       );
-      $this->load->view('authentication/login',$data);
+      $this->load->view('auth/login',$data);
      }
    }
 
